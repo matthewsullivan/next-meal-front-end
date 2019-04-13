fd8aeb6a437a0605aff1f91264fe62ccf8ec9ea0
matthew-sullivan <contact@matthewsullivan.media>
2019-04-12 22:45:47 -0400

    Layout app scaffolding

--- src/next-meal-app/next-meal-app.js
+++ src/next-meal-app/next-meal-app.js
@@ -1,4 +1,13 @@
-import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+import { html, PolymerElement } from "@polymer/polymer/polymer-element.js";
+
+import "@polymer/iron-flex-layout/iron-flex-layout";
+import "@polymer/iron-icon/iron-icon";
+import "@polymer/iron-icons/iron-icons";
+
+import "@polymer/paper-icon-button/paper-icon-button";
+import "@polymer/paper-card/paper-card";
+import "@polymer/paper-checkbox/paper-checkbox";
+import "@polymer/paper-input/paper-input";
 
 /**
  * @customElement
@@ -10,19 +19,65 @@ class NextMealApp extends PolymerElement {
       <style>
         :host {
           display: block;
+          background: url(images/background.jpg);
+          background-position: center;
+          background-repeat: no-repeat;
+          background-size: cover;
+          @apply --layout-center;
+          @apply --layout-center-justified;
+          @apply --layout-fit;
+          @apply --layout-vertical;
+        }
+
+        .location {
+          width: calc(100% - 16px);
+        }
+
+        .location__checkbox {
+          margin-top: 16px;
+        }
+
+        @media only screen and (min-width: 640px) {
+          .location {
+            width: 40%;
+          }
         }
       </style>
-      <h2>Hello [[prop1]]!</h2>
+
+      <paper-card class="location">
+        <div class="card-content">
+          <paper-input label="Type your location">
+            <paper-icon-button
+              class="location__button"
+              disabled$="[[_noLocationSet]]"
+              icon="icons:arrow-forward"
+              on-tap="_handleLocationSubmit"
+              slot="suffix"
+            ></paper-icon-button>
+          </paper-input>
+          <paper-checkbox checked class="location__checkbox"
+            >Remember me</paper-checkbox
+          >
+        </div>
+      </paper-card>
     `;
   }
   static get properties() {
     return {
-      prop1: {
-        type: String,
-        value: 'next-meal-app'
+      _noLocationSet: {
+        type: Boolean,
+        value: true
       }
     };
   }
+
+  /**
+   * Handle Location Submit
+   * @param {objext} event
+   */
+  _handleLocationSubmit(event) {
+    console.log(event);
+  }
 }
 
-window.customElements.define('next-meal-app', NextMealApp);
+window.customElements.define("next-meal-app", NextMealApp);
